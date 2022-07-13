912f3ec9f1632cf7f3a73070415ea9023bea60d4
Livia <lgf21@doc.ic.ac.uk>
2022-07-13 10:20:21 +0100

    add app

---
+++ app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+  <b-input placeholder="No label" rounded v-model="searchBar"></b-input>
+  <b-button type="is-primary is-light" @click="submitSearchForm">Primary Light</b-button>
+  <b-field>
+    <b-input type="textarea"
+      minlength="10"
+      maxlength="100"
+      placeholder="Input text"
+      v-model="searchTextArea">
+    </b-input> 
+  </b-field>
+  <b-button type="is-primary is-light" @click="submitTextForm">Primary Light</b-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      searchBar: '',
+      searchTextArea: '',
+    }
+  },
+  methods: {
+    submitSearchForm() {
+      alert(this.searchBar)
+    },
+    submitTextForm(){
+      alert(this.searchTextArea)
+    }
+  },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 768px;
+  margin: 0 auto; 
+}
+</style>
